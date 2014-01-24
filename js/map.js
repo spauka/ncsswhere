5c30604b69fda65264f88177a0e762cb814426ca
Kenni Bawden <kenni.bawden@nicta.com.au>
2014-01-24 14:52:29 +1100

    Added a blank leaflet map

---
+++ js/map.js
@@ -0,0 +1,7 @@
+$(function() {
+	var map = L.map('map').setView([-31.952162, 145.69519], 4);
+	L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+		maxZoom: 18,
+	}).addTo(map);
+});
